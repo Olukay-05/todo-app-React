9b325d33c11d2a82fd3ce3d8716f90ae1907c6f8
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2022-12-24 20:08:50 +0100

    Initial Commit-2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Ed's Todo List</h1>
+    </header>
+    <Form />
+    </div>
+  );
+}
+
+export default App;
