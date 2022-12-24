bedad452c4bae460a01c43e629c5065cb965bb94
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2022-12-24 23:31:01 +0100

    Form and TodoList Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
-      <h1>Ed's Todo List</h1>
-    </header>
-    <Form />
+        <h1>Kay's Todo List</h1>
+      </header>
+      <Form 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        inputText={inputText}
+      />
+      <TodoList />
     </div>
   );
 }
