9ac65b42919cdb34a48273fe7d219c5d05ea2bbc
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2022-12-25 18:06:44 +0100

    localStorage added

--- src/App.js
+++ src/App.js
@@ -27,8 +27,29 @@ function App() {
       }
     }
 
+
+    const saveLocalTodos = () => {
+      getLocalTodos();
+    };
+
+    const getLocalTodos = () => {
+      if(localStorage.getItem("todos") === null) {
+        localStorage.setItem('todos', JSON.stringify([]));
+      }else {
+        let todoLocal = JSON.parse(localStorage.getItem("todos"))
+          setTodos(todoLocal);
+      }
+    };
+
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
+
+
+
+
+
+  
 
   
 
