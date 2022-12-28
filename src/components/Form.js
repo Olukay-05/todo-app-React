2e135f2e81469446c51f1a79e56873b84fc07bfd
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2022-12-28 19:56:14 +0100

    API Call Bug

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios, * as others from 'axios';
 const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
 
     //Function to update the input
@@ -10,17 +10,29 @@ const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
 
     }
 
-    const submitTodoHandler = (e) => {
+    const submitTodoHandler = async (e) => {
 
         e.preventDefault();
         setTodos([
-            ...todos, 
-            
+            ...todos,
+
             {text: inputText, completed: false, id: Math.random() * 1000}
         ]);
 
+        // axios.post("localhost:8080/todo/add", {content: "Connect application", userName: "tobi"}).then((response => {
+        //     console.log(response.data);
+        // })).catch((err => {
+        //     console.log(err);
+        // }));
         setInputText("");
-        
+        try {
+            const res = await axios.post('http://localhost:8080/todo/add', {content: "Connect application", userName: "tobi"});
+            console.log(res.data);
+        }catch (e) {
+            console.log("axios error");
+            console.log(e);
+        }
+
     };
 
     const statusHandler = (e) => {
